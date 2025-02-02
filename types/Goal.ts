9ca9db00a41c98ee9be3b09b899c8213292a3338
Gilles Pirio <g36130@gmail.com>
2025-02-02 14:21:37 -0800

    chore: cleanup generated code

--- types/Goal.ts
+++
@@ -1,9 +0,0 @@
-export interface Goal {
-  id: number;
-  title: string;
-  description: string;
-  tags: string[];
-  achieveBy: Date;
-  status: "running" | "completed";
-  imageUrl: string | null;
-}
